a3c466ff6ec52e3784a8b63e6ab83b5f5a3d1d16
Wasif Siddique <Wasif.Siddique@nike.com>
2021-04-07 00:31:01 -0700

    implement a text summarizer in python

---
+++ summarizer/summarizer.py
@@ -0,0 +1,20 @@
+###
+### Author: Sornali Rahman
+### Class: CSc 110
+### Description: This program displays a landscape in which a
+###              'vanishing point' may be seen while dragging the
+###              landscape up and down. CHANGE THASFAJD:KLFJADKLFJADKLFJ"IAFLKADJ
+###
+
+def main():
+    file_name = input('enter gamelog file name:')
+    file = open(file_name, 'r')
+    summarize(file)
+
+
+def summarize(file):
+    team_points = []
+
+    print()
+
+main()
